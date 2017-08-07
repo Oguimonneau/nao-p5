65789ee29f0e7f6866180a12680d80e5ebd61766
DESKTOP-S16KD45\oguim <oguimonneau@gmail.com>
2017-08-07 07:55:15 +0200

    Intégration FOSUserBundle "Basic"

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="nao_user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
+
