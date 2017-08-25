9c8875937178721094d3b5bd2e5e4e306a6a2570
Nicolas <clad87ster@gmail.com>
2017-08-25 17:44:43 +0200

    Récupération des observations validées en bdd et affichage du résultat sous forme de tableau

--- src/BackOfficeBundle/Controller/BackController.php
+++ src/BackOfficeBundle/Controller/BackController.php
@@ -13,7 +13,19 @@ class BackController extends Controller
 
     public function observationsListAction()
     {
-    	return $this->render('BackOfficeBundle:Default:observationsList.html.twig');
+    	/**
+    	 * Get all observation to send them in view as a list
+    	 *
+    	 *	@var $repository AppBundle\Repository\ObservationRepository
+    	 */
+    	$em = $this->getDoctrine()->getManager();
+    	$repository = $em->getRepository('AppBundle:Observation');
+
+    	$observationsList = $repository->findByValide(true);
+
+    	return $this->render('BackOfficeBundle:Default:observationsList.html.twig', array(
+    		'observationsList' => $observationsList
+    	));
     }
 
     public function validationListAction()
