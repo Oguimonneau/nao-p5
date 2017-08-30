9098d07ee4bb8395a91f5722d750560ebbb13ced
nassim90 <laouiti90@yahoo.fr>
2017-08-30 22:41:12 +0200

    correction du js

--- web/js/nao-chercher.js
+++ web/js/nao-chercher.js
@@ -1,29 +1,7 @@
-
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {});
-    var geocoder = new google.maps.Geocoder;
-    var infoWindow = new google.maps.InfoWindow({map: map});
-
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-                var pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude,
-                };
-                $(appbundle_observation_latitude).val(pos.lat) ;
-                $(appbundle_observation_longitude).val(pos.lng);
-                infoWindow.setPosition(pos);
-                map.setCenter(pos);
-                geocodeLatLng(geocoder, map, infoWindow);
-                var legend = document.getElementById('legend');
-                legend.innerHTML = '<strong>Votre position</strong> </br>Latitude : ' + pos.lat + ' - Longitude : ' + pos.lng;
-            },
-            function() {
-                handleLocationError(true, infoWindow, map.getCenter());
-            });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
 }
