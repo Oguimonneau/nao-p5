1f3a5d3df2ae66aea10772c8dcba54582cdf9c7d
Olivier Guimonneau <oguimonneau@gmail.com>
2017-09-15 16:36:46 +0200

    Correction détail observation si pas d'image

--- web/js/main.js
+++ web/js/main.js
@@ -1,64 +1,12 @@
-// (function($){
-//     $.fn.scrollingTo = function( opts ) {
-//         var defaults = {
-//             animationTime : 1000,
-//             easing : '',
-//             callbackBeforeTransition : function(){},
-//             callbackAfterTransition : function(){}
-//         };
-//
-//         var config = $.extend( {}, defaults, opts );
-//
-//         $(this).click(function(e){
-//             var eventVal = e;
-//             e.preventDefault();
-//
-//             var $section = $(document).find( $(this).data('section') );
-//             if ( $section.length < 1 ) {
-//                 return false;
-//             };
-//
-//             if ( $('html, body').is(':animated') ) {
-//                 $('html, body').stop( true, true );
-//             };
-//
-//             var scrollPos = $section.offset().top;
-//
-//             if ( $(window).scrollTop() == scrollPos ) {
-//                 return false;
-//             };
-//
-//             config.callbackBeforeTransition(eventVal, $section);
-//
-//             $('html, body').animate({
-//                 'scrollTop' : (scrollPos+'px' )
-//             }, config.animationTime, config.easing, function(){
-//                 config.callbackAfterTransition(eventVal, $section);
-//             });
-//         });
-//     };
-// }(jQuery));
-
-
-
 jQuery(document).ready(function(){
 	"use strict";
 	new WOW().init();
 
 
-// (function(){
-//  jQuery('.smooth-scroll').scrollingTo();
-// }());
 
 });
 
-
-
-
 $(document).ready(function(){
-
-
-
 
     $(window).scroll(function () {
         if ($(window).scrollTop() > 50) {
